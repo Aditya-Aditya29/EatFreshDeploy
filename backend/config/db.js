e54ffd3f6c9dd26c30d8b6f019bfb51f294f50a3
Aditya Aditya <adicanada.1088@gmail.com>
2025-01-07 21:26:38 -0500

    Added Remote DB Link

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/EatFresh', {
+    await mongoose.connect(process.env.DATABASE_URL || 'mongodb://localhost:27017/EatFresh', {
       useNewUrlParser: true,
       useUnifiedTopology: true
     });
